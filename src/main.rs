f065d66331edf4bfe28a2ca455bde1ebf2598f21
Michal <miso.zima@1337tester.com>
2023-01-01 16:28:58 +0000

    Rewritten requests, new libraries

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,37 @@ extern crate log;
 extern crate env_logger;
 mod functions;
 mod micros;
-use crate::micros::microser;
+// use crate::micros::microser;
 
-fn main() {
-    microser();
+use log::{info, error};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+
+async fn get_request(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    info!("Microservice received a request: {:?}", _req);
+    Ok(Response::new("Hello, World".into()))
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+    // Binding the address
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    // A `Service` is needed for every connection, so this
+    // creates one from our `hello_world` function.
+    let make_svc = make_service_fn(|_conn| async {
+        // service_fn converts our function into a `Service`
+        Ok::<_, Infallible>(service_fn(get_request))
+    });
+    info!("Running microservice at {}", addr);
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    // Run this server for... forever!
+    if let Err(e) = server.await {
+        error!("server error: {}", e);
+    }
 }
--- src/micros.rs
+++ src/micros.rs
@@ -1,30 +1,35 @@
-extern crate hyper;
-extern crate futures;
-
-use hyper::server::{Request, Response, Service};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
 use log::info;
-use futures::future::Future;
+use log::error;
 
-struct Microservice;
-
-impl Service for Microservice {
-    type Request = Request;
-    type Response = Response;
-    type Error = hyper::Error;
-    type Future = Box<dyn Future<Item = Self::Response, Error = Self::Error>>;
-
-    fn call(&self, request: Request) -> Self::Future {
-        info!("Microservice received a request: {:?}", request);
-        Box::new(futures::future::ok(Response::new()))
-    }
+async fn get_call(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    info!("Microservice received a request: {:?}", _req);
+    Ok(Response::new(Body::from("Hello World!")))
 }
 
-pub fn microser(){
+pub async fn microser(){
     env_logger::init();
-    let address = "127.0.0.1:8080".parse().unwrap();
-    let server = hyper::server::Http::new()
-        .bind(&address, || Ok(Microservice {}))
-        .unwrap();
-    info!("Running microservice at {}", address);
-    server.run().unwrap();
+    // We'll bind to 127.0.0.1:3000
+    let addr: SocketAddr = ([127, 0, 0, 1], 3000).into();
+
+    // A `Service` is needed for every connection, so this
+    // creates one from our `hello_world` function.
+    // For every connection, we must make a `Service` to handle all
+    // incoming HTTP requests on said connection.
+    let make_svc = make_service_fn(|_conn| {
+        // This is the `Service` that will handle the connection.
+        // `service_fn` is a helper to convert a function that
+        // returns a Response into a `Service`.
+        async { Ok::<_, Infallible>(service_fn(get_call)) }
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    info!("Running microservice at {}", addr);
+    if let Err(e) = server.await {
+        error!("server error: {}", e);
+    }    
 }
